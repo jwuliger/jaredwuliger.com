9469eac0ebdc4818870e00f8505c23461d016a18
Jared Wuliger <hello@jaredwuliger.com>
2023-05-27 14:57:44 -0400

    header, footer, ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { NgModule } from '@angular/core';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, CoreModule],
+    declarations: [AppComponent, HeaderComponent, FooterComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CoreModule,
+        BrowserAnimationsModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
