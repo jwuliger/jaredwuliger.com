a787516576456b021f261b7396ad29aacba76e7a
Jared Wuliger <hello@jaredwuliger.com>
2023-05-27 12:52:19 -0400

    set basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, CoreModule],
     providers: [],
     bootstrap: [AppComponent]
 })
