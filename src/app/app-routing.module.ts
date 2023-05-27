050ffd97c784de2fef765d86b7c5e9d540744db2
Jared Wuliger <hello@jaredwuliger.com>
2023-05-27 13:18:37 -0400

    add feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+    {
+        path: 'about',
+        loadChildren: () =>
+            import('./features/about/about.module').then((m) => m.AboutModule)
+    },
+    {
+        path: 'services',
+        loadChildren: () =>
+            import('./features/services/services.module').then(
+                (m) => m.ServicesModule
+            )
+    },
+    {
+        path: 'projects',
+        loadChildren: () =>
+            import('./features/projects/projects.module').then(
+                (m) => m.ProjectsModule
+            )
+    },
+    {
+        path: 'faq',
+        loadChildren: () =>
+            import('./features/faq/faq.module').then((m) => m.FaqModule)
+    },
+    {
+        path: 'blog',
+        loadChildren: () =>
+            import('./features/blog/blog.module').then((m) => m.BlogModule)
+    },
+    {
+        path: 'contact',
+        loadChildren: () =>
+            import('./features/contact/contact.module').then(
+                (m) => m.ContactModule
+            )
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
