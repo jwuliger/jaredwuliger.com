0e00c45bfdce81b46217e0362115318cfc80a411
Jared Wuliger <hello@jaredwuliger.com>
2023-05-31 09:17:23 -0400

    about page design

--- src/app/features/about/about.module.ts
+++ src/app/features/about/about.module.ts
@@ -1,10 +1,11 @@
 import { AboutComponent } from './about.component';
 import { AboutRoutingModule } from './about-routing.module';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [AboutComponent],
-    imports: [CommonModule, AboutRoutingModule]
+    imports: [CommonModule, AboutRoutingModule, MatButtonModule]
 })
 export class AboutModule {}
