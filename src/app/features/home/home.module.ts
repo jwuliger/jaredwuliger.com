f4a2be93b949ccdfb0386860d4ad2c87cbdc8034
Jared Wuliger <hello@jaredwuliger.com>
2023-06-05 14:02:02 -0400

    add ngx-typed-js for home page

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,9 +3,15 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, HomeRoutingModule, MatButtonModule]
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        MatButtonModule,
+        NgxTypedJsModule
+    ]
 })
 export class HomeModule {}
