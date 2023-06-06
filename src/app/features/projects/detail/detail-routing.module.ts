fd548935777bcd293e21408c333138fefbe71af1
Jared Wuliger <hello@jaredwuliger.com>
2023-06-06 16:23:21 -0400

    project details page

---
+++ src/app/features/projects/detail/detail-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { DetailComponent } from './detail.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: DetailComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class DetailRoutingModule {}
--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NgxMasonryModule } from 'ngx-masonry';
 import { ProjectsComponent } from './projects.component';
 import { ProjectsRoutingModule } from './projects-routing.module';
 
 @NgModule({
     declarations: [ProjectsComponent],
-    imports: [CommonModule, ProjectsRoutingModule, NgxMasonryModule]
+    imports: [CommonModule, ProjectsRoutingModule]
 })
 export class ProjectsModule {}
