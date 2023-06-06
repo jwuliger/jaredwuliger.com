f832c7dbfb0c48851fb42898c50e194a58f2c86c
Jared Wuliger <hello@jaredwuliger.com>
2023-06-06 16:17:40 -0400

    projects start

--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ProjectsRoutingModule } from './projects-routing.module';
+import { NgModule } from '@angular/core';
+import { NgxMasonryModule } from 'ngx-masonry';
 import { ProjectsComponent } from './projects.component';
+import { ProjectsRoutingModule } from './projects-routing.module';
 
 @NgModule({
     declarations: [ProjectsComponent],
-    imports: [CommonModule, ProjectsRoutingModule]
+    imports: [CommonModule, ProjectsRoutingModule, NgxMasonryModule]
 })
 export class ProjectsModule {}
